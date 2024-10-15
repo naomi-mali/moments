e8428748d490a5869462624edb5b1b8653857d70
Timea Mali <derzsi_timea@yahoo.com>
2024-10-15 17:31:33 +0000

    added access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://social-api1-ba63b7ae14c7.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
